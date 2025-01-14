5bd530eee8726d8efec0f00f9409aada7cf2600c
Austin "Paco" Rainwater <hey@paco.wtf>
2025-01-13 19:28:10 -0500

    Custom assertions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,27 +2,73 @@ import pytest_asyncio
 import os
 from homeassistant.core import HomeAssistant
 from typing import AsyncGenerator
-from typing import Any
+from typing import Any, Optional
+
+
+class EntityAssertion:
+    def __init__(self, entity_id: str):
+        self.entity_id = entity_id
+        self.attribute = None
+        self.fn = None
+        self.value_to_check = None
+        # TODO: Set to False if something has changed?
+        self.checked = False
+
+    def attribute(self, attribute: str):
+        self.attribute = attribute
+        return self
+    
+    def equals(self, expected: str):
+        # TODO: Raise an error if fn or expected exists?
+        self.fn = "equals"
+        self.value_to_check = expected
+        return self
+
+    async def check(self, hass: HomeAssistant):
+        if self.checked:
+            return True
+        if self.fn is None:
+            raise ValueError("No assertion function set")
+        state = hass.states.get(self.entity_id)
+        if state is None:
+            raise AssertionError(f"Entity {self.entity_id} not found")
+        if self.attribute is not None:
+            value = state.attributes.get(self.attribute)
+        else:
+            value = state.state
+        if self.fn == "equals":
+            assert value == self.value_to_check, f"Expected entity {self.entity_id} to be {self.value_to_check}, but got {value}"
+        else:
+            raise ValueError(f"Unknown assertion function {self.fn}")
+        self.checked = True
 
 
 class TestHomeAssistant(HomeAssistant):
-    def assert_entity_is(self, sensor: str, attribute: str, expected: str = None):
-        if expected is None:
-            expected = attribute
-            attribute = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.assertions = []
 
-        state = self.states.get(sensor)
-        if attribute:
-            assert state.attributes.get(attribute) == expected
-        else:
-            assert state.state == expected
+    def assert_entity(self, entity_id: str, *, attribute: Optional[str] = None, expected: Optional[str] = None):
+        assertion = EntityAssertion(entity_id)
+        if attribute is not None:
+            assertion.attribute(attribute)
+        if expected is not None:
+            assertion.equals(expected)
+        self.assertions.append(assertion)
+        return assertion
+    
+    async def check_assertions(self):
+        print(f"Checking {len(self.assertions)} assertions")
+        for assertion in self.assertions:
+            await assertion.check(self)
+        self.assertions = []
 
 
 @pytest_asyncio.fixture
-async def hass() -> AsyncGenerator[HomeAssistant]:
+async def hass() -> AsyncGenerator[TestHomeAssistant]:
     hass = TestHomeAssistant(os.path.dirname(os.path.dirname(__file__)))
     await hass.async_start()
     yield hass
-    assert False  # Test failure
+    await hass.check_assertions()
 
     await hass.async_stop()
