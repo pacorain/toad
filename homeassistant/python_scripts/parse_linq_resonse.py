515ad2be6ea88783e7840ba84008bf6bcfe3e010
Austin "Paco" Rainwater <hey@paco.wtf>
2025-02-12 23:32:25 -0500

    Wip: School lunch
    - Pull from URL
    - Parse the response

---
+++ homeassistant/python_scripts/parse_linq_resonse.py
@@ -0,0 +1,24 @@
+"""
+Parse LINQ response
+"""
+print(data)
+
+def get_first(data, key, value):
+    for item in data:
+        if item[key] == value:
+            return item
+    raise ValueError(f'{key} not found in data')
+
+
+lunch = get_first(json_data['FamilyMenuSessions'], 'ServingSession', 'Lunch')
+days = lunch['MenuPlans'][0]['Days']
+
+for day in days:
+    menu = {}
+    recipe_categories = day['MenuMeals'][0]['RecipeCategories']
+    for category in recipe_categories:
+        recipes = category['Recipes']
+        menu[category['CategoryName']] = ', '.join([recipe['RecipeName'] for recipe in recipes])
+
+
+    output[day['Date']] = menu
