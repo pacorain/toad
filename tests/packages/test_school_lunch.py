8fb17b0472fa0aacca38618c42984f73cb76bcba
Austin "Paco" Rainwater <hey@paco.wtf>
2025-02-23 23:34:29 -0500

    Add more unit tests

--- tests/packages/test_school_lunch.py
+++ tests/packages/test_school_lunch.py
@@ -1,3 +1,6 @@
+from mockhass import MockHomeAssistant
+
+
 async def test_parse_school_lunch(hass):
     mock_lunch_response = {
         "FamilyMenuSessions": [
@@ -60,7 +63,7 @@ async def test_parse_school_lunch(hass):
                                             {
                                                 "CategoryName": "Milk",
                                                 "Recipes": [
-                                                    {"RecipeName": "Unwhole Milk"},
+                                                    {"RecipeName": "Hole Milk"},
                                                     {
                                                         "RecipeName": "Void Flavored Milk"
                                                     },
@@ -81,9 +84,53 @@ async def test_parse_school_lunch(hass):
         "1/1/2040": {
             "Main Entree": "Roasted Nothing, Fried Air",
             "Sides": "Faux Fries, Imaginary Salad",
-            "Milk": "Unwhole Milk, Void Flavored Milk",
+            "Milk": "Hole Milk, Void Flavored Milk",
         }
     }
 
-    response = await hass.services.async_call("python_script", "parse_school_lunch", service_data=mock_lunch_response, blocking=True, return_response=True)
+    response = await hass.services.async_call(
+        "python_script",
+        "parse_school_lunch",
+        service_data=mock_lunch_response,
+        blocking=True,
+        return_response=True,
+    )
     assert response == expected_lunch
+
+
+async def test_no_data_returns_empty_dict(hass):
+    mock_lunch_response = {
+        "FamilyMenuSessions": [{"ServingSession": "Lunch", "MenuPlans": [{"Days": []}]}]
+    }
+    response = await hass.services.async_call(
+        "python_script",
+        "parse_school_lunch",
+        service_data=mock_lunch_response,
+        blocking=True,
+        return_response=True,
+    )
+    assert response == {}
+
+
+async def test_school_lunch_event_updates_sensors(hass: MockHomeAssistant):
+    event_data = {
+        "daily_lunches": {
+            "1/1/2040": {
+                "Main Entree": "Roasted Nothing, Fried Air",
+                "Sides": "Faux Fries, Imaginary Salad",
+                "Milk": "Hole Milk, Void Flavored Milk",
+            }
+        },
+        "first_date": "1/1/2040",
+    }
+
+    async with hass:
+        hass.bus.async_fire("school_lunch_updated", event_data)
+        await hass.async_block_till_done()
+        hass.assert_entity("sensor.next_school_lunch_date").equals("1/1/2040")
+        hass.assert_entity("sensor.next_school_lunch_entrees").equals(
+            "Roasted Nothing, Fried Air"
+        )
+        hass.assert_entity("sensor.next_school_lunch_sides").equals(
+            "Faux Fries, Imaginary Salad"
+        )
