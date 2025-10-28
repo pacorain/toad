87c80a4ef4da90530960afe0e5574534638d2f1c
Austin "Paco" Rainwater <hey@paco.wtf>
2025-10-28 07:54:06 -0400

    Upgrade home assistant

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='mockhass',
-    version='0.1',
+    version='0.2.1',
     description='Framework for testing Home Assistant configurations',
     packages=['mockhass'],
     package_dir={'mockhass': 'src/python/mockhass'},
-    install_requires=['pytest==8.3.4', 'homeassistant==2025.1.2', 'pytest-asyncio==0.25.2', 'pytest-xdist==3.6.1'],
+    install_requires=['pytest==8.4.2', 'homeassistant==2025.10.4', 'pytest-asyncio==1.2.0', 'pytest-xdist==3.8.0'],
 )
